12d53675c6520440cd2233c39b5556a059227294
Mjait <mohammed.jait99@gmail.com>
2026-04-11 14:28:29 +0100

    to push at gitea

--- geometrical_shapes/Main.java
+++ geometrical_shapes/Main.java
@@ -17,7 +17,7 @@ public class Main {
         }
 
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 30; i++) {
             Pentagon pentagon = Pentagon.random(image.getWidth(), image.getHeight());
             pentagon.draw(image);
         }
